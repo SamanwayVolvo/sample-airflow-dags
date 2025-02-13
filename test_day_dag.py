464016e08b5481214084f701b26ad78dadf002c8
Harpreet <allenkallz@gmail.com>
2025-02-13 21:53:01 +0100

    Update test_day_dag.py

--- test_day_dag.py
+++ test_day_dag.py
@@ -20,11 +20,11 @@ from sqlalchemy.orm import exc
 from typing import Any, Dict, List, Union, cast
 
 access_control = {
-    # "cld-dc-golden-path" : {
+    "cld-dc-golden-path" : {
     # "cld-data-launchpad-sg" : {
-    #     "DAG Runs": {'can_read','can_edit', 'can_create'},
-    #     "DAGs": {'can_read','can_edit', 'can_create'},
-    # }
+        "DAG Runs": {'can_read','can_edit', 'can_create'},
+        "DAGs": {'can_read','can_edit', 'can_create'},
+    }
 }
 
 # @dag(start_date=datetime(2024, 2, 2), schedule_interval='@daily', catchup=False)
